13ac0ddd064277e160f0f142b227478ce0263027
roshan <roshan@feyzilogic.com>
2021-09-02 11:20:41 -0400

    change TF code

--- main.tf
+++ main.tf
@@ -21,25 +21,3 @@ resource "aws_iam_policy" "basically_allow_all" {
 }
 EOF
 }
-
-resource "aws_iam_policy" "basically_deny_all" {
-  name        = "some_policy"
-  path        = "/"
-  description = "Some policy with a long description that denies anything"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Deny",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
